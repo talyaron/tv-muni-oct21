ec65910b645624297870d82490f4e3344003fcf7
Tal Yaron <tal.yaron@gmail.com>
2022-11-03 11:01:19 +0200

    WIP

---
+++ MUI-React/mui-demo/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button variant="contained">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
